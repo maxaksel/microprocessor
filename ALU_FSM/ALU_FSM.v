3c48d062ff4befadbd300e48a2612704b9be0525
Joshua Luke Stidham <jls32@jade.clear.rice.edu>
2023-02-20 09:26:15 -0600

    Fixed a few issues with state transitions in FSM

--- ALU_FSM/ALU_FSM.v
+++ ALU_FSM/ALU_FSM.v
@@ -22,8 +22,11 @@ output wire [2:0] state_out;
 // States
 parameter IDLE = 3'b000, P = 3'b001, Z = 3'b010, N = 3'b100;
 
-wire       alpha, beta, gamma;
-reg  [2:0] current_state, next_state;
+
+wire        alpha, beta, gamma;
+reg  [2:0]  current_state, next_state;
+reg         we_latch, reset_latch, br_latch;
+reg [2:0]   dec_in_latch;
 
 // internal combo logic
 assign state_out = current_state;
@@ -33,33 +36,31 @@ assign gamma     = ~n_alu_in & ~z_alu_in & p_alu_in & we_reg_in;
 
 // State update logic
 always @(negedge clka) begin
-    if (reset_in) begin
-        next_state <= IDLE;
-    end else begin
-        case ({alpha, beta, gamma})
-            N : next_state <= N;
-            Z : next_state <= Z;
-            P : next_state <= P;
-            default: next_state <= IDLE;
-        endcase
-    end
+    we_latch <= we_reg_in;
+    reset_latch <= reset_in;
+    br_latch <= br_in;
+    dec_in_latch <= {n_dec_in, z_dec_in, p_dec_in};
+    case ({alpha, beta, gamma})
+        N : next_state <= N;
+        Z : next_state <= Z;
+        P : next_state <= P;
+        default: next_state <= IDLE;
+    endcase
 end
 
 // State output logic
 always @(negedge clkb) begin
-    current_state <= next_state;
-    case (next_state)
-        N : begin
-            pc_ctl_0_out <= n_dec_in & br_in;
-        end
-        Z : begin
-            pc_ctl_0_out <= z_dec_in & br_in;
-        end
-        P : begin
-            pc_ctl_0_out <= p_dec_in & br_in;
-        end
-        default: pc_ctl_0_out <= 0;
-    endcase
+    if (reset_latch == 1'b1) begin
+        current_state <= IDLE;
+        pc_ctl_0_out <= 1'b0;
+    end else if (we_latch == 1'b1) begin
+        current_state <= next_state;
+        pc_ctl_0_out <= (|(next_state & dec_in_latch)) & br_latch;
+    end else begin
+        current_state <= current_state;
+        pc_ctl_0_out <= (|(current_state & dec_in_latch)) & br_latch;
+    end
+
 end
     
 endmodule
