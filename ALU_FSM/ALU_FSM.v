750388d6d9e36d4e17ddd6835ff6ba0bfa4a2a57
Joshua Luke Stidham <jls32@amber.clear.rice.edu>
2023-02-22 05:47:12 -0600

    initial source code modifications for 2-CYCLE FSM

--- ALU_FSM/ALU_FSM.v
+++ ALU_FSM/ALU_FSM.v
@@ -11,56 +11,71 @@ module ALU_FSM (
         we_reg_in,
         br_in,
         pc_ctl_0_out,
-        state_out,);
+        pc_update_clkedge,
+        state_out);
 
     input wire clka, clkb, reset_in, n_dec_in, z_dec_in, p_dec_in,
                 n_alu_in, z_alu_in, p_alu_in, we_reg_in, br_in;
     output reg pc_ctl_0_out;
-    output wire [2:0] state_out;
+    output wire pc_update_clkedge;
+    output wire [3:0] state_out;
 
     // States
-    parameter IDLE = 3'b000, P = 3'b001, Z = 3'b010, N = 3'b100;
+    parameter IDLE_PC = 4'b0000, P_PC = 4'b0001, Z_PC = 4'b0010, N_PC = 4'b0100;
+    parameter IDLE_NPC = 4'b1000, P_NPC = 4'b1001, Z_NPC = 4'b1010, N_NPC = 4'b1100;
 
 
     wire        alpha, beta, gamma;
-    reg  [2:0]  current_state, next_state;
+    reg  [3:0]  current_state, next_state;
     reg         we_latch, reset_latch, br_latch;
+    reg pc_update;
     reg [2:0]   dec_in_latch;
     wire [2:0]  br_curr_state, br_next_state;
 
     // internal combo logic
-    assign state_out = current_state;
+    assign pc_update_clkedge = pc_update;
+    assign state_out = {pc_update_clkedge, current_state};
     assign alpha     = n_alu_in & ~z_alu_in & ~p_alu_in & we_reg_in;
     assign beta      = ~n_alu_in & z_alu_in & ~p_alu_in & we_reg_in;
     assign gamma     = ~n_alu_in & ~z_alu_in & p_alu_in & we_reg_in;
-    assign br_curr_state = current_state & dec_in_latch;
-    assign br_next_state = next_state & dec_in_latch;
+    assign br_curr_state = current_state[2:0] & dec_in_latch;
+    assign br_next_state = next_state[2:0] & dec_in_latch;
 
     // State update logic
     always @(negedge clka) begin
+
         we_latch <= we_reg_in;
         reset_latch <= reset_in;
         br_latch <= br_in;
         dec_in_latch <= {n_dec_in, z_dec_in, p_dec_in};
         case ({alpha, beta, gamma})
-            N : next_state <= N;
-            Z : next_state <= Z;
-            P : next_state <= P;
-            default: next_state <= IDLE;
+            N : next_state <= N_PC;
+            Z : next_state <= Z_PC;
+            P : next_state <= P_PC;
+            default: next_state <= IDLE_PC;
         endcase
     end
 
     // State output logic
     always @(negedge clkb) begin
         if (reset_latch == 1'b1) begin
-            current_state <= IDLE;
+            current_state <= IDLE_NPC;
             pc_ctl_0_out <= 1'b0;
         end else if (we_latch == 1'b1) begin
-            current_state <= next_state;
-            pc_ctl_0_out <= (|br_next_state) && br_latch;
+            if (current_state[3] == 1'b1) begin
+                current_state <= next_state;
+                pc_ctl_0_out <= (|br_next_state) && br_latch;
+            end else begin
+                current_state <= {1'b0, current_state[2:0]};
+            end
         end else begin
-            current_state <= current_state;
-            pc_ctl_0_out <= (|br_curr_state) && br_latch;
+            if (current_state[3] == 1'b1) begin
+                current_state <= {1'b1, current_state[2:0]};
+                pc_ctl_0_out <= (|br_curr_state) && br_latch;
+            end else begin
+                current_state <= {1'b0, current_state[2:0]};
+            end
+            
         end
     end
     
