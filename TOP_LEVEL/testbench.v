53a74138ea6d2aff32f6bd145cec93c924812c7f
Joshua Luke Stidham <jls32@amber.clear.rice.edu>
2023-02-28 14:05:14 -0600

    finished writing initial test module

--- TOP_LEVEL/testbench.v
+++ TOP_LEVEL/testbench.v
@@ -35,6 +35,221 @@ module toplevel_testbench();
                 clka = 0; clkb = 1; #10;
 
                 reset = 0; //deassert reset
+                we_ins = 1;
+                instruction_load = 16'b0001000000101000; // PC = 0
+
+                clka = 0; clkb = 0; #10;
+                clka = 1; clkb = 0; #10;
+                clka = 0; clkb = 0; #10;
+                clka = 0; clkb = 1; #10;
+
+                clka = 0; clkb = 0; #10;
+                clka = 1; clkb = 0; #10;
+                clka = 0; clkb = 0; #10;
+                clka = 0; clkb = 1; #10;
+
+                instruction_load = 16'b0001001001100001; // PC = 2
+
+                clka = 0; clkb = 0; #10;
+                clka = 1; clkb = 0; #10;
+                clka = 0; clkb = 0; #10;
+                clka = 0; clkb = 1; #10;
+
+                clka = 0; clkb = 0; #10;
+                clka = 1; clkb = 0; #10;
+                clka = 0; clkb = 0; #10;
+                clka = 0; clkb = 1; #10;
+
+                instruction_load = 16'b1001001001000000; // PC = 4
+
+                clka = 0; clkb = 0; #10;
+                clka = 1; clkb = 0; #10;
+                clka = 0; clkb = 0; #10;
+                clka = 0; clkb = 1; #10;
+
+                clka = 0; clkb = 0; #10;
+                clka = 1; clkb = 0; #10;
+                clka = 0; clkb = 0; #10;
+                clka = 0; clkb = 1; #10;
+
+                instruction_load = 16'b0001001001100001; // PC = 6
+
+                clka = 0; clkb = 0; #10;
+                clka = 1; clkb = 0; #10;
+                clka = 0; clkb = 0; #10;
+                clka = 0; clkb = 1; #10;
+
+                clka = 0; clkb = 0; #10;
+                clka = 1; clkb = 0; #10;
+                clka = 0; clkb = 0; #10;
+                clka = 0; clkb = 1; #10;
+
+                instruction_load = 16'b0001000000000001; // PC = 8
+
+                clka = 0; clkb = 0; #10;
+                clka = 1; clkb = 0; #10;
+                clka = 0; clkb = 0; #10;
+                clka = 0; clkb = 1; #10;
+
+                clka = 0; clkb = 0; #10;
+                clka = 1; clkb = 0; #10;
+                clka = 0; clkb = 0; #10;
+                clka = 0; clkb = 1; #10;
+
+                instruction_load = 16'b0000001000111110; // PC = 10
+
+                clka = 0; clkb = 0; #10;
+                clka = 1; clkb = 0; #10;
+                clka = 0; clkb = 0; #10;
+                clka = 0; clkb = 1; #10;
+
+                clka = 0; clkb = 0; #10;
+                clka = 1; clkb = 0; #10;
+                clka = 0; clkb = 0; #10;
+                clka = 0; clkb = 1; #10;
+
+                instruction_load = 16'b0; // PC = 12, NOP
+
+                clka = 0; clkb = 0; #10;
+                clka = 1; clkb = 0; #10;
+                clka = 0; clkb = 0; #10;
+                clka = 0; clkb = 1; #10;
+
+                clka = 0; clkb = 0; #10;
+                clka = 1; clkb = 0; #10;
+                clka = 0; clkb = 0; #10;
+                clka = 0; clkb = 1; #10;
+
+                reset = 1;
+                we_ins = 0; // deassert we_ins
+
+                clka = 0; clkb = 0; #10;
+                clka = 1; clkb = 0; #10;
+                clka = 0; clkb = 0; #10;
+                clka = 0; clkb = 1; #10;
+
+                reset = 0; //reset done, start of PC = 0
+
+                clka = 0; clkb = 0; #10;
+                clka = 1; clkb = 0; #10;
+                clka = 0; clkb = 0; #10;
+                clka = 0; clkb = 1; #10;
+
+                clka = 0; clkb = 0; #10;
+                clka = 1; clkb = 0; #10;
+                clka = 0; clkb = 0; #10;
+                clka = 0; clkb = 1; #10;
+
+                // PC = 2
+
+                clka = 0; clkb = 0; #10;
+                clka = 1; clkb = 0; #10;
+                clka = 0; clkb = 0; #10;
+                clka = 0; clkb = 1; #10;
+
+                clka = 0; clkb = 0; #10;
+                clka = 1; clkb = 0; #10;
+                clka = 0; clkb = 0; #10;
+                clka = 0; clkb = 1; #10;
+
+                // PC = 4
+
+                clka = 0; clkb = 0; #10;
+                clka = 1; clkb = 0; #10;
+                clka = 0; clkb = 0; #10;
+                clka = 0; clkb = 1; #10;
+
+                clka = 0; clkb = 0; #10;
+                clka = 1; clkb = 0; #10;
+                clka = 0; clkb = 0; #10;
+                clka = 0; clkb = 1; #10;
+
+                // PC = 6
+
+                clka = 0; clkb = 0; #10;
+                clka = 1; clkb = 0; #10;
+                clka = 0; clkb = 0; #10;
+                clka = 0; clkb = 1; #10;
+
+                clka = 0; clkb = 0; #10;
+                clka = 1; clkb = 0; #10;
+                clka = 0; clkb = 0; #10;
+                clka = 0; clkb = 1; #10;
+
+                // PC = 8
+
+                clka = 0; clkb = 0; #10;
+                clka = 1; clkb = 0; #10;
+                clka = 0; clkb = 0; #10;
+                clka = 0; clkb = 1; #10;
+
+                clka = 0; clkb = 0; #10;
+                clka = 1; clkb = 0; #10;
+                clka = 0; clkb = 0; #10;
+                clka = 0; clkb = 1; #10;
+
+                // PC = 10
+
+                clka = 0; clkb = 0; #10;
+                clka = 1; clkb = 0; #10;
+                clka = 0; clkb = 0; #10;
+                clka = 0; clkb = 1; #10;
+
+                clka = 0; clkb = 0; #10;
+                clka = 1; clkb = 0; #10;
+                clka = 0; clkb = 0; #10;
+                clka = 0; clkb = 1; #10;
+
+                // PC = 8
+
+                clka = 0; clkb = 0; #10;
+                clka = 1; clkb = 0; #10;
+                clka = 0; clkb = 0; #10;
+                clka = 0; clkb = 1; #10;
+
+                clka = 0; clkb = 0; #10;
+                clka = 1; clkb = 0; #10;
+                clka = 0; clkb = 0; #10;
+                clka = 0; clkb = 1; #10;
+
+                // PC = 10
+
+                clka = 0; clkb = 0; #10;
+                clka = 1; clkb = 0; #10;
+                clka = 0; clkb = 0; #10;
+                clka = 0; clkb = 1; #10;
+
+                clka = 0; clkb = 0; #10;
+                clka = 1; clkb = 0; #10;
+                clka = 0; clkb = 0; #10;
+                clka = 0; clkb = 1; #10;
+
+                // PC = 8
+
+                clka = 0; clkb = 0; #10;
+                clka = 1; clkb = 0; #10;
+                clka = 0; clkb = 0; #10;
+                clka = 0; clkb = 1; #10;
+
+                clka = 0; clkb = 0; #10;
+                clka = 1; clkb = 0; #10;
+                clka = 0; clkb = 0; #10;
+                clka = 0; clkb = 1; #10;
+
+                // PC = 10
+
+                clka = 0; clkb = 0; #10;
+                clka = 1; clkb = 0; #10;
+                clka = 0; clkb = 0; #10;
+                clka = 0; clkb = 1; #10;
+
+                clka = 0; clkb = 0; #10;
+                clka = 1; clkb = 0; #10;
+                clka = 0; clkb = 0; #10;
+                clka = 0; clkb = 1; #10;
+
+                // STOP, loop should still be in progress here
+                
 
                 $dumpfile ("toplelevel_testbench.vcd");
                 $dumpvars;
