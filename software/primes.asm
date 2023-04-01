3c82011a9396db4d7dd04f3882b84f0c299ff80d
Josh Stidham <jls32@jade.clear.rice.edu>
2023-04-01 10:48:40 -0500

    correct assembly and binary for primes

--- software/primes.asm
+++ software/primes.asm
@@ -8,16 +8,16 @@ addi $3 $3 #1
 lea $7 #6 ; store return address
 lea $6 :subtract ; load address of subtract into R6
 jmp $6 ; subtract 1 from R0
-:loop addi $1 $0 #0 ; move R0 to R1 (loop counter)
-addi $0 $4 #0 ; R1 is loop counter R4 is value, move value into R0
-lea $7 #4 ; load return address
-jmp $6 ; subtract R0 = R0 - R1 (R0 = val - loop counter)
-brz :done ; done
+:loop addi $1 $0 #0 ; move R0 to R1 (loop counter) 16
+addi $0 $4 #0 ; R1 is loop counter R4 is value, move value into R0 18
+lea $7 #4 ; load return address 20
+jmp $6 ; subtract R0 = R0 - R1 (R0 = val - loop counter) 22
+brz :done ; done 24
 brp :subtract ; sub again return to R7 plus 4
 brn :else
-:subtract addi $0 $0 #1 ; subtract R1 from R0. Uses R0 R1 and R2 Return Address in R7
-not $2 $1
-add $0 $0 $2
+:subtract addi $0 $0 #1 ; subtract R1 from R0. Uses R0 R1 and R2 Return Address in R7 30 
+not $2 $1 ; 32
+add $0 $0 $2 ; 34
 jmp $7 ; return address
 :else addi $0 $1 #0
 addi $1 $3 #0 ; 1 into r1
